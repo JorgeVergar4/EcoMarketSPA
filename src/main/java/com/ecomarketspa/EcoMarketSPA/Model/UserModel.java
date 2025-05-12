5a4fab49032043a471cfff5f1fd55fcc3b44c132
Jorge Vergara <jor.vergaras@duocuc.cl>
2025-05-12 17:42:42 -0400

    Creacion de modelo usuario

---
+++ src/main/java/com/ecomarketspa/EcoMarketSPA/Model/UserModel.java
@@ -0,0 +1,26 @@
+package com.ecomarketspa.EcoMarketSPA.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "users_table")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String login;
+
+    String password;
+
+    String email;
+
+
+
+}
