046112e1d6ce2a0ad088e9cbe63aded9d8408241
Boryot <jvstuardo@gmail.com>
2025-07-02 16:51:46 -0400

    cambio de ruta de redireccion a swagger a index

--- src/main/java/com/ecomarketspa/EcoMarketSPA/Controller/HomeController.java
+++ src/main/java/com/ecomarketspa/EcoMarketSPA/Controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String redirectToSwagger() {
-        return "redirect:/swagger-ui/index.html";
+    public String home() {
+        return "index";
     }
 }
